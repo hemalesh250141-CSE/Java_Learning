ed3ac0a274339bd03d411c9f53c3c9cfe5b177bb
hemalesh250141-CSE <hemalesh.250141@cse.ritchennai.edu.in>
2026-09-24 12:53:30 +0530

    Create  Basic Java Program Structure.java

---
+++ Basic Java Program Structure.java
@@ -0,0 +1,7 @@
+public class HelloWorld { 
+Main method – Entry point of any Java program 
+public static void main(String[] args) { 
+Print statement 
+System.out.println("Hello, World!"); 
+} 
+} 
