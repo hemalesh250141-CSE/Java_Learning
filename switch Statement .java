1ac24e1909640cb98d0edc02933a4669fb98c00f
hemalesh250141-CSE <hemalesh.250141@cse.ritchennai.edu.in>
2026-09-24 13:03:05 +0530

    Create switch Statement .java

---
+++ switch Statement .java
@@ -0,0 +1,25 @@
+public class ConditionExample { 
+public static void main(String[] args) { 
+int score = 72; 
+// if-else if-else ladder 
+if (score >= 90) { 
+System.out.println("Excellent"); 
+} else if (score >= 60) { 
+System.out.println("Passed"); 
+} else { 
+System.out.println("Failed"); 
+} 
+// switch example 
+int option = 2; 
+switch (option) { 
+case 1: 
+System.out.println("Option One"); 
+break; 
+case 2: 
+System.out.println("Option Two"); 
+break; 
+default: 
+System.out.println("Other Option"); 
+} 
+} 
+} 
