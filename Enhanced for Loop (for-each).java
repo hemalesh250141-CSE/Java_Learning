630b11351c848db389223ef31aa4006f0720ebb5
hemalesh250141-CSE <hemalesh.250141@cse.ritchennai.edu.in>
2026-09-24 13:04:52 +0530

    Create  Enhanced for Loop (for-each).java

---
+++ Enhanced for Loop (for-each).java
@@ -0,0 +1,23 @@
+public class LoopExample { 
+public static void main(String[] args) { 
+// for loop 
+System.out.println("For loop:"); 
+for (int i = 1; i <= 3; i++) { 
+System.out.println("i = " + i); 
+} 
+// while loop 
+System.out.println("While loop:"); 
+int j = 1; 
+while (j <= 3) { 
+System.out.println("j = " + j); 
+j++; 
+} 
+// do-while loop 
+System.out.println("Do-While loop:"); 
+int k = 1; 
+do { 
+System.out.println("k = " + k); 
+k++; 
+} while (k <= 3); 
+} 
+} 
