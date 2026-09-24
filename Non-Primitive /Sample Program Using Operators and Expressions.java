6bbc3a4dffdbb00ac8d74b1feab299f00854302b
hemalesh250141-CSE <hemalesh.250141@cse.ritchennai.edu.in>
2026-09-24 12:56:08 +0530

    Add sample program using operators and expressions

---
+++ Non-Primitive /Sample Program Using Operators and Expressions.java
@@ -0,0 +1,15 @@
+public class OperatorExample { 
+public static void main(String[] args) { 
+int a = 10, b = 3; 
+int sum = a + b; 
+int product = a * b; 
+int quotient = a / b; 
+int remainder = a % b; 
+boolean result = (a > b) && (b != 0); 
+System.out.println("Sum: " + sum); 
+System.out.println("Product: " + product); 
+System.out.println("Quotient: " + quotient); 
+System.out.println("Remainder: " + remainder); 
+System.out.println("Is a > b and b != 0? " + result); 
+} 
+}
