7caa714af092df6bd9f12a4a44a610db7954c04a
hemalesh250141-CSE <hemalesh.250141@cse.ritchennai.edu.in>
2026-09-25 10:24:13 +0530

    Create Reference Data Types.java

---
+++ Reference Data Types.java
@@ -0,0 +1,14 @@
+public class VariableExample { 
+public static void main(String[] args) { 
+int age = 18; 
+double weight = 65.5; 
+char gender = 'M'; 
+boolean isAdult = true; 
+String name = "Ravi"; 
+System.out.println("Name: " + name); 
+System.out.println("Age: " + age); 
+System.out.println("Weight: " + weight); 
+System.out.println("Gender: " + gender); 
+System.out.println("Is Adult: " + isAdult); 
+} 
+}
