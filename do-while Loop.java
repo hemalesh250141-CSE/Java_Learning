3adb140e7cb4254b061bd2c5e692ca009db44e4f
hemalesh250141-CSE <hemalesh.250141@cse.ritchennai.edu.in>
2026-09-26 10:01:10 +0530

    Document do-while loop syntax and example
    Add example and syntax for do-while loop in Java.

---
+++ do-while Loop.java
@@ -0,0 +1,11 @@
+✅ Syntax: 
+do { 
+// code to repeat 
+} while (condition); 
+️ Example: 
+int i = 1; 
+do { 
+System.out.println("Number: " + i); 
+i++; 
+} while (i <= 5); 
+⚠️ Runs at least once even if condition is false. 
